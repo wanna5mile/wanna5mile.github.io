445c66a1c2222581f70df5228b3971f225a784fa
R5 <kitloverbs90210@gmail.com>
2025-10-04 16:22:42 -0500

    Update carousel.js

--- system/js/carousel.js
+++ system/js/carousel.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Dynamically determine correct JSON path
   // If your HTML is in root, use "system/json/assets-test.json"
   // If it's in /system/pages/, use "../json/assets-test.json"
-  const jsonPath = location.pathname.includes("/system/pages/")
+  const jsonPath = location.pathname.includes("/system/")
     ? "../json/assets-test.json"
     : "system/json/assets-test.json";
 
